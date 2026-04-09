62744f3c1d8dfd0729ecb0882da5c612be362876
Zvasilv <Zvasilv@inbox.ru>
2026-04-10 02:49:19 +0500

    Очень много if

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Пиши оно само решит");
+string input = Console.ReadLine();
+string[] parts = input.Split(' '); // нужно да бы  разбить пример на первое число, знак и второе число
+float a = float.Parse(parts[0]); // первое число
+string znak = parts[1]; // знак
+float b = float.Parse(parts[2]); // втрое число
+if (znak == "+") {Console.WriteLine(a + b); }
+if (znak == "-") { Console.WriteLine(a - b); }
+if (znak == "*") { Console.WriteLine(a * b); }
+if (znak == "/" && b != 0) { Console.WriteLine(a / b); }
+if (znak == "/" && b == 0) { Console.WriteLine("Деление на 0 невозможно"); }
+if (znak == "^") Console.WriteLine(Math.Pow(a, b));
