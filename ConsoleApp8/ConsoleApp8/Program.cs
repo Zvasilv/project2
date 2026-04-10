73df38221d0b2e544111a739a29ca6040f953cce
Zvasilv <Zvasilv@inbox.ru>
2026-04-10 17:07:06 +0500

    Без букв?

--- ConsoleApp8/ConsoleApp8/Program.cs
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -1,21 +1,36 @@
-﻿Console.WriteLine("Пиши оно само решит");
-string input = Console.ReadLine().Replace(" ", "");
-char[] simvol = { '+', '-', '*', '/', '^' };
-string[] parts = input.Split(simvol); // с помощью этой штуки, программа понимает, когда нужно отделить циферки от знака. Сам знак не читает. 
-float a = float.Parse(parts[0]); // первое число
-float b = float.Parse(parts[1]); // второе число
-string znak = "";
-foreach (char simvol2 in simvol) // находит знак и трансформирует его в символ.
+﻿using System.Reflection.Metadata;
+
+
+while (true)
 {
-    if (input.Contains(simvol2))
+    Console.WriteLine("Напишите выражение");
+    string input = Console.ReadLine().Replace(" ", "");
+    char[] simvol = { '+', '-', '*', '/', '^' };
+    string[] parts = input.Split(simvol); // с помощью этой штуки, программа понимает, когда нужно отделить циферки от знака. Сам знак не читает. 
+    bool a = float.TryParse(parts[0], out float c); // первое число
+    bool b = float.TryParse(parts[1], out float d); // второе число
+    if (!a || !b) // Проверяет на буквы, есть - перезапуск. Нет - продолжает программу
     {
-        znak = simvol2.ToString();
-        break;
+        Console.WriteLine("Без букв"); 
+        continue;
+    }    
+    string znak = "";
+    foreach (char simvol2 in simvol) // находит знак и трансформирует его в символ.
+    {
+        if (input.Contains(simvol2))
+        {
+            znak = simvol2.ToString();
+            break;
+        }
     }
+    
+    if (znak == "+") { Console.WriteLine(c + d); }
+    if (znak == "-") { Console.WriteLine(c - d); }
+    if (znak == "*") { Console.WriteLine(c * d); }
+    if (znak == "/" && d != 0) { Console.WriteLine(c / d); }
+    if (znak == "/" && d == 0) { Console.WriteLine("Деление на 0 невозможно"); }
+    if (znak == "^") Console.WriteLine(Math.Pow(c, d));
+    break;
+    
 }
-if (znak == "+") { Console.WriteLine(a + b); }
-if (znak == "-") { Console.WriteLine(a - b); }
-if (znak == "*") { Console.WriteLine(a * b); }
-if (znak == "/" && b != 0) { Console.WriteLine(a / b); }
-if (znak == "/" && b == 0) { Console.WriteLine("Деление на 0 невозможно"); }
-if (znak == "^") Console.WriteLine(Math.Pow(a, b));
+
