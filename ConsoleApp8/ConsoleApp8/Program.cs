dca785a07cf5637b5a3ac68c8ac06b3bf68d4ae2
Zvasilv <Zvasilv@inbox.ru>
2026-04-14 22:58:14 +0500

    Последние правки

--- ConsoleApp8/ConsoleApp8/Program.cs
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -5,17 +5,20 @@ using System.Security.Cryptography.X509Certificates;
 while (true)
 {
     Console.WriteLine("Напишите выражение");
-    string input = Console.ReadLine().Replace(" ", null); 
-    char[] simvol = { '+', '-', '*', '/', '^' }; 
+    string input = Console.ReadLine().Replace(" ", null);
+    char[] simvol = { '+', '-', '*', '/', '^' };
     input.IndexOfAny(simvol); // ищет есть ли символы. Он же индекс. 
     int index = input.IndexOfAny(simvol);
+
     if (index == -1) continue;
-    char znak = input[index];
-    string[] parts = input.Split(simvol); // с помощью этой штуки, программа понимает, когда нужно отделить циферки от знака. Сам знак не читает.
-    if (float.TryParse(parts[0], out float c) && float.TryParse(parts[1], out float d)) // Проверяет на буквы, есть - перезапуск. Нет - продолжает программу.
+
+    char znak = input[index]; // Берем всё, что до знака и всё, что после.
+    string part1 = input.Substring(0, index);
+    string part2 = input.Substring(index + 1);
+    if (float.TryParse(part1, out float c) && float.TryParse(part2, out float d))
     {
         float res = 0;
-        int f = 0;
+        int f = 6;
         switch (znak)
         {
             case '+':
@@ -28,14 +31,14 @@ while (true)
                 res = c * d;
                 break;
             case '/':
-                if (d != 0) 
-                { 
-                    res = c / d;                   
+                if (d != 0)
+                {
+                    res = c / d;
                     Console.WriteLine("Сколько знаков после запятой вы хотите увидеть?");
                     f = int.Parse(Console.ReadLine());
                 }
-                if (d == 0) 
-                { 
+                if (d == 0)
+                {
                     Console.WriteLine("Деление на 0 невозможно");
                     continue;
                 }
@@ -44,13 +47,13 @@ while (true)
                 res = (float)Math.Pow(c, d);
                 break;
 
-        }      
+        }
         res = float.Round(res, f);
         Console.WriteLine($"Результат: {res}");
     }
-    else 
+    else
     {
-           Console.WriteLine("Без букв");
-           continue;
-    }      
+        Console.WriteLine("Без букв");
+        continue;
+    }
 }
