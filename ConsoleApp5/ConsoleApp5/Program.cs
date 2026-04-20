d92ea1ceec915720c44fbdbb70ec4f1eb98485f6
Zvasilv <Zvasilv@inbox.ru>
2026-04-21 01:28:18 +0500

    Возведение в степень и без букв.

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+string d = Console.ReadLine();
+double[] res = new double[d.Length];
+for (int i = 0; i < d.Length; i++)
+{
+    if (!char.IsDigit(d[i])) { Console.WriteLine($"Ошибка: '{d[i]}' - не цифра"); continue; }
+    int a = (int)char.GetNumericValue(d[i]);
+    res[i] = Math.Pow(a, i);
+    Console.WriteLine(res[i]);
+}
