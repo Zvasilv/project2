f1a88906bb393334d2423bdd6d6ea1e8c01de52f
Zvasilv <Zvasilv@inbox.ru>
2026-04-22 18:17:52 +0500

    Упрощение

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,10 +1,8 @@
 ﻿Console.WriteLine("Введите число:");
 string d = Console.ReadLine();
-double[] res = new double[d.Length];
 for (int i = 0; i < d.Length; i++)
 {
-    if (!char.IsDigit(d[i])) { Console.WriteLine($"Ошибка '{d[i]}' - не цифра"); continue; }
-    int a = (int)char.GetNumericValue(d[i]);
-    res[i] = Math.Pow(a, i);
-    Console.WriteLine(res[i]);
+    if (!int.TryParse(d[i].ToString(), out int a)) 
+        { Console.WriteLine($"Ошибка '{d[i]}' - не цифра"); continue; }
+    Console.WriteLine(Math.Pow(a, i));
 }
